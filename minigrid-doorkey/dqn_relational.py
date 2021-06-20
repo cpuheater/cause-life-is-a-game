91f2e76cb065e9a044d0ffcb6704f902592c0adf
Luke <cpuheater@gmail.com>
2021-06-20 10:38:32 +0200

    add dqn-relational

--- minigrid-doorkey/dqn_relational.py
+++ minigrid-doorkey/dqn_relational.py
@@ -6,6 +6,7 @@ import gym
 from gym import spaces
 import cv2
 cv2.ocl.setUseOpenCL(False)
+from einops import rearrange
 
 class ImageToPyTorch(gym.ObservationWrapper):
     """
@@ -97,7 +98,7 @@ if __name__ == "__main__":
                         help='the name of this experiment')
     parser.add_argument('--gym-id', type=str, default="MiniGrid-DoorKey-5x5-v0",
                         help='the id of the gym environment')
-    parser.add_argument('--learning-rate', type=float, default=2e-4,
+    parser.add_argument('--learning-rate', type=float, default=0.0005,
                         help='the learning rate of the optimizer')
     parser.add_argument('--seed', type=int, default=2,
                         help='seed of the experiment')
@@ -117,13 +118,13 @@ if __name__ == "__main__":
                         help="the entity (team) of wandb's project")
 
     # Algorithm specific arguments
-    parser.add_argument('--buffer-size', type=int, default=10000000,
+    parser.add_argument('--buffer-size', type=int, default=9000,
                         help='the replay memory buffer size')
     parser.add_argument('--gamma', type=float, default=0.99,
                         help='the discount factor gamma')
-    parser.add_argument('--target-network-frequency', type=int, default=1000,
+    parser.add_argument('--target-network-frequency', type=int, default=100,
                         help="the timesteps it takes to update the target network")
-    parser.add_argument('--max-grad-norm', type=float, default=0.5,
+    parser.add_argument('--max-grad-norm', type=float, default=1.0,
                         help='the maximum norm for the gradient clipping')
     parser.add_argument('--batch-size', type=int, default=32,
                         help="the batch size of sample from the reply memory")
@@ -153,6 +154,7 @@ if args.prod_mode:
 
 # TRY NOT TO MODIFY: seeding
 device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
+#device = "cpu"
 env = gym.make(args.gym_id)
 env = wrap_env(env)
 env = gym.wrappers.RecordEpisodeStatistics(env) # records episode reward in `info['episode']['r']`
@@ -198,6 +200,11 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
+def prepare_state(x):
+    maxv = x.flatten().max()
+    x = x / maxv
+    return x
+
 # ALGO LOGIC: initialize agent here:
 class Scale(nn.Module):
     def __init__(self, scale):
@@ -206,6 +213,86 @@ class Scale(nn.Module):
 
     def forward(self, x):
         return x * self.scale
+
+class MultiHeadRelationalModule(torch.nn.Module):
+    def __init__(self):
+        super(MultiHeadRelationalModule, self).__init__()
+        self.conv1_ch = 16
+        self.conv2_ch = 20
+        self.conv3_ch = 24
+        self.conv4_ch = 30
+        self.H = 28
+        self.W = 28
+        self.node_size = 64
+        self.lin_hid = 100
+        self.out_dim = 5
+        self.ch_in = 3
+        self.sp_coord_dim = 2
+        self.N = int(7 ** 2)
+        self.n_heads = 3
+
+        self.conv1 = nn.Conv2d(self.ch_in, self.conv1_ch, kernel_size=(1, 1), padding=0)  # A
+        self.conv2 = nn.Conv2d(self.conv1_ch, self.conv2_ch, kernel_size=(1, 1), padding=0)
+        self.proj_shape = (self.conv2_ch + self.sp_coord_dim, self.n_heads * self.node_size)
+        self.k_proj = nn.Linear(*self.proj_shape)
+        self.q_proj = nn.Linear(*self.proj_shape)
+        self.v_proj = nn.Linear(*self.proj_shape)
+
+        self.k_lin = nn.Linear(self.node_size, self.N)  # B
+        self.q_lin = nn.Linear(self.node_size, self.N)
+        self.a_lin = nn.Linear(self.N, self.N)
+
+        self.node_shape = (self.n_heads, self.N, self.node_size)
+        self.k_norm = nn.LayerNorm(self.node_shape, elementwise_affine=True)
+        self.q_norm = nn.LayerNorm(self.node_shape, elementwise_affine=True)
+        self.v_norm = nn.LayerNorm(self.node_shape, elementwise_affine=True)
+
+        self.linear1 = nn.Linear(self.n_heads * self.node_size, self.node_size)
+        self.norm1 = nn.LayerNorm([self.N, self.node_size], elementwise_affine=False)
+        self.linear2 = nn.Linear(self.node_size, self.out_dim)
+
+    def forward(self, x):
+        N, Cin, H, W = x.shape
+        x = self.conv1(x)
+        x = torch.relu(x)
+        x = self.conv2(x)
+        x = torch.relu(x)
+        with torch.no_grad():
+            self.conv_map = x.clone()  # C
+        _, _, cH, cW = x.shape
+        xcoords = torch.arange(cW).repeat(cH, 1).float().to(device) / cW
+        ycoords = torch.arange(cH).repeat(cW, 1).transpose(1, 0).float().to(device) / cH
+        spatial_coords = torch.stack([xcoords, ycoords], dim=0)
+        spatial_coords = spatial_coords.unsqueeze(dim=0)
+        spatial_coords = spatial_coords.repeat(N, 1, 1, 1)
+        x = torch.cat([x, spatial_coords], dim=1)
+        x = x.permute(0, 2, 3, 1)
+        x = x.flatten(1, 2)
+
+        K = rearrange(self.k_proj(x), "b n (head d) -> b head n d", head=self.n_heads)
+        K = self.k_norm(K)
+
+        Q = rearrange(self.q_proj(x), "b n (head d) -> b head n d", head=self.n_heads)
+        Q = self.q_norm(Q)
+
+        V = rearrange(self.v_proj(x), "b n (head d) -> b head n d", head=self.n_heads)
+        V = self.v_norm(V)
+        A = torch.nn.functional.elu(self.q_lin(Q) + self.k_lin(K))  # D
+        A = self.a_lin(A)
+        A = torch.nn.functional.softmax(A, dim=3)
+        with torch.no_grad():
+            self.att_map = A.clone()  # E
+        E = torch.einsum('bhfc,bhcd->bhfd', A, V)  # F
+        E = rearrange(E, 'b head n d -> b n (head d)')
+        E = self.linear1(E)
+        E = torch.relu(E)
+        E = self.norm1(E)
+        E = E.max(dim=1)[0]
+        y = self.linear2(E)
+        y = torch.nn.functional.elu(y)
+        return y
+
+
 class QNetwork(nn.Module):
     def __init__(self, env, frames=3):
         super(QNetwork, self).__init__()
@@ -230,9 +317,17 @@ def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
     slope =  (end_e - start_e) / duration
     return max(slope * t + start_e, end_e)
 
+action_map = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 5,
+}
+
 rb = ReplayBuffer(args.buffer_size)
-q_network = QNetwork(env).to(device)
-target_network = QNetwork(env).to(device)
+q_network = MultiHeadRelationalModule().to(device)
+target_network = MultiHeadRelationalModule().to(device)
 target_network.load_state_dict(q_network.state_dict())
 optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate)
 loss_fn = nn.MSELoss()
@@ -240,20 +335,22 @@ print(device.__repr__())
 print(q_network)
 
 # TRY NOT TO MODIFY: start the game
-obs = env.reset()
+obs = prepare_state(env.reset())
 episode_reward = 0
 for global_step in range(args.total_timesteps):
     # ALGO LOGIC: put action logic here
-    epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction*args.total_timesteps, global_step)
-    obs = np.array(obs)
+    epsilon = 0.5#linear_schedule(args.start_e, args.end_e, args.exploration_fraction*args.total_timesteps, global_step)
+    #obs = np.array(obs)
     if random.random() < epsilon:
-        action = env.action_space.sample()
+        action = int(torch.randint(0, 5, size=(1,)).squeeze())
     else:
-        logits = q_network.forward(obs.reshape((1,)+obs.shape), device)
+        logits = q_network.forward(torch.Tensor(obs.reshape((1,)+obs.shape)).to(device))
         action = torch.argmax(logits, dim=1).tolist()[0]
 
     # TRY NOT TO MODIFY: execute the game and log data.
-    next_obs, reward, done, info = env.step(action)
+    next_obs, reward, done, info = env.step(action_map[action])
+    next_obs = prepare_state(next_obs)
+    reward = -0.01 if reward == 0 else reward
     episode_reward += reward
 
     # TRY NOT TO MODIFY: record rewards for plotting purposes
@@ -267,9 +364,9 @@ for global_step in range(args.total_timesteps):
     if global_step > args.learning_starts and global_step % args.train_frequency == 0:
         s_obs, s_actions, s_rewards, s_next_obses, s_dones = rb.sample(args.batch_size)
         with torch.no_grad():
-            target_max = torch.max(target_network.forward(s_next_obses, device), dim=1)[0]
+            target_max = torch.max(target_network.forward(torch.Tensor(s_next_obses).to(device)), dim=1)[0]
             td_target = torch.Tensor(s_rewards).to(device) + args.gamma * target_max * (1 - torch.Tensor(s_dones).to(device))
-        old_val = q_network.forward(s_obs, device).gather(1, torch.LongTensor(s_actions).view(-1,1).to(device)).squeeze()
+        old_val = q_network.forward(torch.Tensor(s_obs).to(device)).gather(1, torch.LongTensor(s_actions).view(-1,1).to(device)).squeeze()
         loss = loss_fn(td_target, old_val)
 
         if global_step % 100 == 0:
@@ -278,7 +375,7 @@ for global_step in range(args.total_timesteps):
         # optimize the midel
         optimizer.zero_grad()
         loss.backward()
-        nn.utils.clip_grad_norm_(list(q_network.parameters()), args.max_grad_norm)
+        nn.utils.clip_grad_norm_(q_network.parameters(), args.max_grad_norm)
         optimizer.step()
 
         # update the target network
@@ -291,7 +388,7 @@ for global_step in range(args.total_timesteps):
         # important to note that because `EpisodicLifeEnv` wrapper is applied,
         # the real episode reward is actually the sum of episode reward of 5 lives
         # which we record through `info['episode']['r']` provided by gym.wrappers.RecordEpisodeStatistics
-        obs, episode_reward = env.reset(), 0
+        obs, episode_reward = prepare_state(env.reset()), 0
 
 env.close()
 writer.close()
