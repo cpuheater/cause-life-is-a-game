0d31753fd81706f27d977704d295634ade0b7062
Luke <cpuheater@gmail.com>
2021-11-11 12:09:39 +0100

    sth

--- microrts8x8/ppo_gridnet_larger_model.py
+++ microrts8x8/ppo_gridnet_larger_model.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     # Common arguments
     parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
                         help='the name of this experiment')
-    parser.add_argument('--gym-id', type=str, default="Microrts10-workerRushAI",
+    parser.add_argument('--gym-id', type=str, default="Microrts8-workerRushAI",
                         help='the id of the gym environment')
     parser.add_argument('--learning-rate', type=float, default=2.5e-4,
                         help='the learning rate of the optimizer')
