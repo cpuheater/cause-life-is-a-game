a0393071dbcde891b354341289dd858beeea03aa
Luke <cpuheater@gmail.com>
2022-03-22 22:14:17 +0100

    add ddqn

--- vizdoom_basic/ddqn.py
+++ vizdoom_basic/ddqn.py
@@ -4,91 +4,60 @@ import numpy as np
 from collections import deque
 import gym
 from gym import spaces
-import cv2
-cv2.ocl.setUseOpenCL(False)
-
-class ImageToPyTorch(gym.ObservationWrapper):
-    """
-    Image shape to channels x weight x height
-    """
-
-    def __init__(self, env):
-        super(ImageToPyTorch, self).__init__(env)
-        old_shape = self.observation_space.shape
-        self.observation_space = gym.spaces.Box(
-            low=0,
-            high=255,
-            shape=(old_shape[-1], old_shape[0], old_shape[1]),
-            dtype=np.uint8,
-        )
-
-    def observation(self, observation):
-        return np.transpose(observation, axes=(2, 0, 1))
-
-def wrap_env(env):
-    env = InfoWrapper(env)
-    env = WarpFrame(env)
-    env = ImageToPyTorch(env)
-    return env
-
-class WarpFrame(gym.ObservationWrapper):
-    def __init__(self, env, width=84, height=84):
-        super().__init__(env)
-        self.observation_space = env.observation_space.spaces['image']
-
-    def observation(self, obs):
-        return obs
-
-class InfoWrapper(gym.Wrapper):
-    def __init__(self, env):
-        gym.Wrapper.__init__(self, env)
-        self._rewards = []
-
-    def reset(self, **kwargs):
-        obs = self.env.reset(**kwargs)
-        self._rewards = []
-        return obs["image"]
-
-    def step(self, action):
-        obs, reward, done, info = self.env.step(action)
-        self._rewards.append(reward)
-        vis_obs = obs["image"]
-        if done:
-            info = {"reward": sum(self._rewards),
-                    "length": len(self._rewards)}
-
-        return vis_obs, reward, done, info
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-
+import cv2
+cv2.ocl.setUseOpenCL(False)
 import argparse
 from distutils.util import strtobool
 import collections
 import numpy as np
 import gym
 from gym.wrappers import TimeLimit, Monitor
+from vizdoom import DoomGame, Mode, ScreenFormat, ScreenResolution
 from gym.spaces import Discrete, Box, MultiBinary, MultiDiscrete, Space
 import time
 import random
 import os
 from gym_minigrid.wrappers import *
+import itertools
+
+def initialize_vizdoom(config):
+    game = DoomGame()
+    game.load_config(config)
+    #game.set_window_visible(False)
+    game.set_mode(Mode.PLAYER)
+    game.set_screen_format(ScreenFormat.CRCGCB)
+    game.set_screen_resolution(ScreenResolution.RES_160X120)
+    game.init()
+    return game
+
+def preprocess(img, resolution):
+    img = img.transpose((1, 2, 0))
+    return cv2.resize(img, resolution).astype(np.float32).transpose((2,0,1))
+
+class Scale(nn.Module):
+    def __init__(self, scale):
+        super().__init__()
+        self.scale = scale
+
+    def forward(self, x):
+        return x * self.scale
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DQN agent')
     # Common arguments
     parser.add_argument('--exp-name', type=str, default=os.path.basename(__file__).rstrip(".py"),
                         help='the name of this experiment')
-    parser.add_argument('--gym-id', type=str, default="MiniGrid-DoorKey-5x5-v0",
-                        help='the id of the gym environment')
-    parser.add_argument('--learning-rate', type=float, default=7e-4,
+    parser.add_argument('--learning-rate', type=float, default=0.00025,
                         help='the learning rate of the optimizer')
     parser.add_argument('--seed', type=int, default=2,
                         help='seed of the experiment')
-    parser.add_argument('--total-timesteps', type=int, default=4000000,
+    parser.add_argument('--total-timesteps', type=int, default=100000,
                         help='total timesteps of the experiments')
     parser.add_argument('--torch-deterministic', type=lambda x:bool(strtobool(x)), default=True, nargs='?', const=True,
                         help='if toggled, `torch.backends.cudnn.deterministic=False`')
@@ -108,28 +77,28 @@ if __name__ == "__main__":
                         help='the replay memory buffer size')
     parser.add_argument('--gamma', type=float, default=0.99,
                         help='the discount factor gamma')
-    parser.add_argument('--target-network-frequency', type=int, default=100,
+    parser.add_argument('--target-network-frequency', type=int, default=1000,
                         help="the timesteps it takes to update the target network")
     parser.add_argument('--max-grad-norm', type=float, default=0.5,
                         help='the maximum norm for the gradient clipping')
-    parser.add_argument('--batch-size', type=int, default=32,
+    parser.add_argument('--batch-size', type=int, default=64,
                         help="the batch size of sample from the reply memory")
     parser.add_argument('--start-e', type=float, default=1.0,
                         help="the starting epsilon for exploration")
     parser.add_argument('--end-e', type=float, default=0.02,
                         help="the ending epsilon for exploration")
-    parser.add_argument('--exploration-fraction', type=float, default=0.10,
+    parser.add_argument('--exploration-fraction', type=float, default=0.4,
                         help="the fraction of `total-timesteps` it takes from start-e to go end-e")
-    parser.add_argument('--learning-starts', type=int, default=80000,
+    parser.add_argument('--learning-starts', type=int, default=800,
                         help="timestep to start learning")
-    parser.add_argument('--train-frequency', type=int, default=1,
+    parser.add_argument('--train-frequency', type=int, default=4,
                         help="the frequency of training")
     args = parser.parse_args()
-    if not args.seed:
-        args.seed = int(time.time())
+    #if not args.seed:
+    args.seed = int(time.time())
 
 # TRY NOT TO MODIFY: setup the environment
-experiment_name = f"{args.gym_id}__{args.exp_name}__{args.seed}__{int(time.time())}"
+experiment_name = f"{args.exp_name}__{args.seed}__{int(time.time())}"
 writer = SummaryWriter(f"runs/{experiment_name}")
 writer.add_text('hyperparameters', "|param|value|\n|-|-|\n%s" % (
     '\n'.join([f"|{key}|{value}|" for key, value in vars(args).items()])))
@@ -140,21 +109,17 @@ if args.prod_mode:
 
 # TRY NOT TO MODIFY: seeding
 device = torch.device('cuda' if torch.cuda.is_available() and args.cuda else 'cpu')
-env = gym.make(args.gym_id)
-env = wrap_env(env)
-env = gym.wrappers.RecordEpisodeStatistics(env) # records episode reward in `info['episode']['r']`
-if args.capture_video:
-    env = Monitor(env, f'videos/{experiment_name}')
+frame_repeat = 4
+resolution = (64, 112)
+frames = 3
+game = initialize_vizdoom("./scenarios/basic.cfg")
+n = game.get_available_buttons_size()
+actions = [list(a) for a in itertools.product([0, 1], repeat=n)]
 
 random.seed(args.seed)
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 torch.backends.cudnn.deterministic = args.torch_deterministic
-env.seed(args.seed)
-env.action_space.seed(args.seed)
-env.observation_space.seed(args.seed)
-# respect the default timelimit
-assert isinstance(env.action_space, Discrete), "only discrete action space is supported"
 
 # modified from https://github.com/seungeunrho/minimalRL/blob/master/dqn.py#
 class ReplayBuffer():
@@ -185,27 +150,25 @@ def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.constant_(layer.bias, bias_const)
     return layer
 
-def prepare_state(x):
-    maxv = x.flatten().max()
-    x = x / maxv
-    return x
-
 # ALGO LOGIC: initialize agent here:
 class QNetwork(nn.Module):
-    def __init__(self, env, frames=3):
+    def __init__(self, actions, frames=3):
         super(QNetwork, self).__init__()
         self.network = nn.Sequential(
-            layer_init(nn.Conv2d(frames, 16, kernel_size=(1, 1), padding=0)),
+            Scale(1 / 255),
+            layer_init(nn.Conv2d(frames, 32, 8, stride=4)),
             nn.ReLU(),
-            layer_init(nn.Conv2d(16, 20, kernel_size=(1, 1), padding=0)),
+            layer_init(nn.Conv2d(32, 64, 4, stride=2)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, 3, stride=1)),
             nn.ReLU(),
             nn.Flatten(),
-            layer_init(nn.Linear(980, 256)),
+            layer_init(nn.Linear(2560, 512)),
             nn.ReLU(),
-            nn.Linear(256, 5)
+            nn.Linear(512, len(actions))
         )
 
-    def forward(self, x, device):
+    def forward(self, x):
         x = torch.Tensor(x).to(device)
         return self.network(x)
 
@@ -213,11 +176,10 @@ def linear_schedule(start_e: float, end_e: float, duration: int, t: int):
     slope =  (end_e - start_e) / duration
     return max(slope * t + start_e, end_e)
 
-action_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 5}
 
 rb = ReplayBuffer(args.buffer_size)
-q_network = QNetwork(env).to(device)
-target_network = QNetwork(env).to(device)
+q_network = QNetwork(actions, frames).to(device)
+target_network = QNetwork(actions, frames).to(device)
 target_network.load_state_dict(q_network.state_dict())
 optimizer = optim.Adam(q_network.parameters(), lr=args.learning_rate)
 loss_fn = nn.MSELoss()
@@ -225,37 +187,37 @@ print(device.__repr__())
 print(q_network)
 
 # TRY NOT TO MODIFY: start the game
-obs = prepare_state(env.reset())
+game.new_episode()
 episode_reward = 0
+obs = game.get_state().screen_buffer
+obs = preprocess(obs, resolution)
 for global_step in range(args.total_timesteps):
     # ALGO LOGIC: put action logic here
     epsilon = linear_schedule(args.start_e, args.end_e, args.exploration_fraction*args.total_timesteps, global_step)
-    obs = np.array(obs)
     if random.random() < epsilon:
-        action = int(torch.randint(0, 5, size=(1,)).squeeze())
+        action = torch.tensor(random.randint(0, len(actions) - 1)).long().item()
     else:
-        logits = q_network.forward(obs.reshape((1,)+obs.shape), device)
+        logits = q_network.forward(obs.reshape((1,)+obs.shape))
         action = torch.argmax(logits, dim=1).tolist()[0]
 
     # TRY NOT TO MODIFY: execute the game and log data.
-    next_obs, reward, done, info = env.step(action_map[action])
-    next_obs = prepare_state(next_obs)
-    episode_reward += reward
+    reward = game.make_action(actions[action], frame_repeat)
+    reward *= 0.01
+    done = game.is_episode_finished()
 
-    # TRY NOT TO MODIFY: record rewards for plotting purposes
-    if 'episode' in info.keys():
-        print(f"global_step={global_step}, episode_reward={info['episode']['r']}")
-        writer.add_scalar("charts/episode_reward", info['episode']['r'], global_step)
+    next_obs = preprocess(np.zeros((3, resolution[0], resolution[1])), resolution) if done else preprocess(game.get_state().screen_buffer, resolution)
+
+    if done:
+        writer.add_scalar("charts/episode_reward", game.get_total_reward(), global_step)
         writer.add_scalar("charts/epsilon", epsilon, global_step)
 
-    # ALGO LOGIC: training.
     rb.put((obs, action, reward, next_obs, done))
     if global_step > args.learning_starts and global_step % args.train_frequency == 0:
         s_obs, s_actions, s_rewards, s_next_obses, s_dones = rb.sample(args.batch_size)
         with torch.no_grad():
-            target_max = torch.max(target_network.forward(s_next_obses, device), dim=1)[0]
+            target_max = torch.max(target_network.forward(s_next_obses), dim=1)[0]
             td_target = torch.Tensor(s_rewards).to(device) + args.gamma * target_max * (1 - torch.Tensor(s_dones).to(device))
-        old_val = q_network.forward(s_obs, device).gather(1, torch.LongTensor(s_actions).view(-1,1).to(device)).squeeze()
+        old_val = q_network.forward(s_obs).gather(1, torch.LongTensor(s_actions).view(-1,1).to(device)).squeeze()
         loss = loss_fn(td_target, old_val)
 
         if global_step % 100 == 0:
@@ -264,7 +226,7 @@ for global_step in range(args.total_timesteps):
         # optimize the midel
         optimizer.zero_grad()
         loss.backward()
-        nn.utils.clip_grad_norm_(list(q_network.parameters()), args.max_grad_norm)
+        #nn.utils.clip_grad_norm_(list(q_network.parameters()), args.max_grad_norm)
         optimizer.step()
 
         # update the target network
@@ -272,12 +234,12 @@ for global_step in range(args.total_timesteps):
             target_network.load_state_dict(q_network.state_dict())
 
     # TRY NOT TO MODIFY: CRUCIAL step easy to overlook
-    obs = next_obs
     if done:
-        # important to note that because `EpisodicLifeEnv` wrapper is applied,
-        # the real episode reward is actually the sum of episode reward of 5 lives
-        # which we record through `info['episode']['r']` provided by gym.wrappers.RecordEpisodeStatistics
-        obs, episode_reward = prepare_state(env.reset()), 0
+        game.new_episode()
+        obs, episode_reward = game.get_state().screen_buffer, 0
+        obs = preprocess(obs, resolution)
+    else:
+        obs = next_obs
 
-env.close()
+
 writer.close()
