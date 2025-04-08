e06fee0d68c89634730d8769ce1ab0040b7ddcd3
luke <cpuheater@gmail.com>
2025-04-08 17:43:42 +0200

    add trxl

--- minigrid_doorkey_5x5/ppo_flat_obs.py
+++ minigrid_doorkey_5x5/ppo_flat_obs.py
@@ -34,14 +34,6 @@ def make_env(env_id, idx, capture_video, run_name):
     return thunk
 
 # ALGO LOGIC: initialize agent here:
-class Scale(nn.Module):
-    def __init__(self, scale):
-        super().__init__()
-        self.scale = scale
-
-    def forward(self, x):
-        return x * self.scale
-
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0):
     torch.nn.init.orthogonal_(layer.weight, std)
     torch.nn.init.constant_(layer.bias, bias_const)
