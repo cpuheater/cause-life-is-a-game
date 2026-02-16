fa3eb89830fc23d13f3483bc244e905f2c1e464b
luke <cpuheater@gmail.com>
2026-02-16 11:29:48 +0100

    sth

--- vizdoom_basic/ppo.py
+++ vizdoom_basic/ppo.py
@@ -140,7 +140,7 @@ class ViZDoomEnv(gymnasium.Env):
             info['reward'] = self.total_reward
             info['length'] = self.total_length
 
-        return self.state, reward, done, done, info
+        return self.state, reward, done, done, {}
 
     def reset(self, *, seed: Optional[int] = None, options: Optional[dict] = None):
         super().reset(seed=seed)
@@ -218,16 +218,17 @@ def create_env() -> ViZDoomEnv:
         game.set_window_visible(True)
         game.init()
         # Wrap the game with the Gym adapter.
-        return ViZDoomEnv(game, channels=args.channels)
+        env = gymnasium.wrappers.RecordEpisodeStatistics(ViZDoomEnv(game, channels=args.channels))
+        return env 
     return thunk
 
 #envs = VecPyTorch(DummyVecEnv([create_env(**kwargs) for i in range(args.num_envs)]), device)
 
-envs = VecPyTorch(
-         SubprocVecEnv([create_env() for i in range(args.num_envs)], "fork"),
-         device
-     )
-assert isinstance(envs.action_space, gymnasium.spaces.discrete.Discrete), "only discrete action space is supported"
+
+envs = gymnasium.vector.SyncVectorEnv(
+        [create_env() for i in range(args.num_envs)],
+    )
+assert isinstance(envs.single_action_space, gymnasium.spaces.discrete.Discrete), "only discrete action space is supported"
 
 # ALGO LOGIC: initialize agent here:
 class Scale(nn.Module):
@@ -258,7 +259,7 @@ class Agent(nn.Module):
             layer_init(nn.Linear(2560, 512)),
             nn.ReLU()
         )
-        self.actor = layer_init(nn.Linear(512, envs.action_space.n), std=0.01)
+        self.actor = layer_init(nn.Linear(512, envs.single_action_space.n), std=0.01)
         self.critic = layer_init(nn.Linear(512, 1), std=1)
 
     def forward(self, x):
@@ -283,8 +284,8 @@ if args.anneal_lr:
     lr = lambda f: f * args.learning_rate
 
 # ALGO Logic: Storage for epoch data
-obs = torch.zeros((args.num_steps, args.num_envs) + envs.observation_space.shape).to(device)
-actions = torch.zeros((args.num_steps, args.num_envs) + envs.action_space.shape).to(device)
+obs = torch.zeros((args.num_steps, args.num_envs) + envs.single_observation_space.shape).to(device)
+actions = torch.zeros((args.num_steps, args.num_envs) + envs.single_action_space.shape).to(device)
 logprobs = torch.zeros((args.num_steps, args.num_envs)).to(device)
 rewards = torch.zeros((args.num_steps, args.num_envs)).to(device)
 dones = torch.zeros((args.num_steps, args.num_envs)).to(device)
@@ -295,7 +296,8 @@ global_step = 0
 start_time = time.time()
 # Note how `next_obs` and `next_done` are used; their usage is equivalent to
 # https://github.com/ikostrikov/pytorch-a2c-ppo-acktr-gail/blob/84a7582477fb0d5c82ad6d850fe476829dddd2e1/a2c_ppo_acktr/storage.py#L60
-next_obs = envs.reset()
+next_obs, _ = envs.reset()
+next_obs = torch.Tensor(next_obs).to(device)
 next_done = torch.zeros(args.num_envs).to(device)
 num_updates = args.total_timesteps // args.batch_size
 for update in range(1, num_updates+1):
@@ -320,20 +322,24 @@ for update in range(1, num_updates+1):
         logprobs[step] = logproba
 
         # TRY NOT TO MODIFY: execute the game and log data.
-        next_obs, rs, ds, infos = envs.step(action)
-        rewards[step], next_done = rs.view(-1), torch.Tensor(ds).to(device)
+        next_obs, rs, terminations, truncations, infos = envs.step(action)
+        next_done = np.logical_or(terminations, truncations)
+        next_obs = torch.Tensor(next_obs).to(device)
+        rewards[step], next_done =torch.tensor(rs).to(device).view(-1), torch.Tensor(next_done).to(device)
 
         #for info in infos:
-        #    if 'episode' in info.keys():
-        #        print(f"global_step={global_step}, episode_reward={info['episode']['r']}")
-        #        writer.add_scalar("charts/episode_reward", info['episode']['r'], global_step)
-        #        break
-        for info in infos:
-            if 'reward' in info.keys():
-                print(f"global_step={global_step}, episodic_return={info['reward']}")
-                writer.add_scalar("charts/episodic_return", info['reward'], global_step)
-            if 'length' in info.keys():
-                writer.add_scalar("charts/episodic_length", info['length'], global_step)
+        #    if 'reward' in info.keys():
+        #        print(f"global_step={global_step}, episodic_return={info['reward']}")
+        #        writer.add_scalar("charts/episodic_return", info['reward'], global_step)
+        #    if 'length' in info.keys():
+        #        writer.add_scalar("charts/episodic_length", info['length'], global_step)
+        
+        if "final_info" in infos:
+            for info in infos["final_info"]:
+                if info and "episode" in info:
+                    print(f"global_step={global_step}, episodic_return={info['episode']['r']}")
+                    writer.add_scalar("charts/episodic_return", info["episode"]["r"], global_step)
+                    writer.add_scalar("charts/episodic_length", info["episode"]["l"], global_step)        
 
     # bootstrap reward if not done. reached the batch limit
     with torch.no_grad():
@@ -364,9 +370,9 @@ for update in range(1, num_updates+1):
             advantages = returns - values
 
     # flatten the batch
-    b_obs = obs.reshape((-1,)+envs.observation_space.shape)
+    b_obs = obs.reshape((-1,)+envs.single_observation_space.shape)
     b_logprobs = logprobs.reshape(-1)
-    b_actions = actions.reshape((-1,)+envs.action_space.shape)
+    b_actions = actions.reshape((-1,)+envs.single_action_space.shape)
     b_advantages = advantages.reshape(-1)
     b_returns = returns.reshape(-1)
     b_values = values.reshape(-1)
